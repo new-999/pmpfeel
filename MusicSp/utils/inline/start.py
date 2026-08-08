8396089ad7421c567328669af8de1f88125a1227
new-999 <khithlainhtet2023@gmail.com>
2026-08-08 17:29:39 +0630

    Update start.py

--- MusicSp/utils/inline/start.py
+++ MusicSp/utils/inline/start.py
@@ -29,6 +29,6 @@ def private_panel(_):
             InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
             InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_GROUP),
         ],
-        [InlineKeyboardButton(text=_["S_B_5"], url=f"tg://openmessage?user_id={OWNER_ID}")],
+        [InlineKeyboardButton(text="❍ᴡηєʀ", url=)],
     ]
     return buttons
